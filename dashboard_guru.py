a9144cc9241bfbd44b3262d1f29e185ddcfbfc23
tiaraaa644 <mutiarannisa877@gmail.com>
2025-01-31 19:24:22 +0700

    Update dashboard_guru.py

--- dashboard_guru.py
+++ dashboard_guru.py
@@ -15,10 +15,10 @@ import logging
 def create_connection():
     try:
         connection = mysql.connector.connect(
-            host="localhost",        # Ganti dengan host database Anda
-            user="root",             # Ganti dengan username database Anda
-            password="",             # Ganti dengan password database Anda
-            database="db_prediksi_beasiswa"   # Ganti dengan nama database Anda
+            host=st.secrets["DB_HOST"],
+            database=st.secrets["DB_DATABASE"],
+            user=st.secrets["DB_USER"],
+            password=st.secrets["DB_PASSWORD"]
         )
         return connection
     except mysql.connector.Error as err:

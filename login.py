5d721df1fcbd32eb312b89c4d0bf6f54807ea262
tiaraaa644 <mutiarannisa877@gmail.com>
2025-01-31 19:23:43 +0700

    Update login.py

--- login.py
+++ login.py
@@ -7,10 +7,10 @@ import importlib  # Untuk memuat file lain secara dinamis
 def create_connection():
     try:
         connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="db_prediksi_beasiswa"
+            host=st.secrets["DB_HOST"],
+            database=st.secrets["DB_DATABASE"],
+            user=st.secrets["DB_USER"],
+            password=st.secrets["DB_PASSWORD"]
         )
         return connection
     except Error as e:
